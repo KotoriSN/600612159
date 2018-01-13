58e5b5d81df87d394cf74d3543394280ad013e10
Pichchai <32808238+Pichchai@users.noreply.github.com>
2018-01-13 17:27:04 +0700

    Add source code
    Add Source.cpp

---
+++ Source.cpp
@@ -0,0 +1,10 @@
+﻿#include<iostream>
+using namespace std;
+
+int main() {
+	cout << "   ∧＿_∧";
+	cout << " （ ´・ω・`)    (○)  Thanks….";
+	cout << ", (ヽ∩∩ノ), 、ヽ | 〃 Nepu……..";
+	cout << " “““ ““ ““ ““ ““ ““ ““ ““““ ““";
+	return 0;
+}	
